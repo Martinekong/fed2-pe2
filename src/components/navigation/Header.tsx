76cbf1aa086de4916a3b33f474268baba68a397d
Martine <martinekongsrud@outlook.com>
2026-02-08 21:16:07 +0100

    #15 More contrast on VenueCardSkeleton

--- src/components/navigation/Header.tsx
+++ src/components/navigation/Header.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { Link, NavLink, useLocation } from 'react-router-dom';
 
 import headerBg from '../../assets/header-bg.jpg';
